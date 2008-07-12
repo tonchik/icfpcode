5b479e8e7f5a85036c8b7595982232dcc8f8b3f0
OriginalNull@gmail.com <originalnull@gmail.com>
2008-07-12 15:22:44 +0400

    mqueue

---
+++ code/mqueue.py
@@ -0,0 +1,32 @@
+from thread import *
+from Queue import *
+from threading import *
+
+QueueMaxSize = 10000
+
+class MQueue:
+    def __init__(self, limit):
+        self.queue = []
+        self.lock  = RLock()
+        self.nemptyevent = Event()
+        self.nemptyevent.clear()
+        if limit <= 0 :
+            self.limit = QueueMaxSize
+        else :
+            self.limit = limit
+    def put(self, obj):
+        self.lock.acquire(True)
+        if len(self.queue) == self.limit :
+            self.queue.pop(0)
+        self.queue.append(obj)
+        self.nemptyevent.set()
+        self.lock.release()
+
+    def get(self):
+        self.nemptyevent.wait()
+        self.lock.acquire(True)
+        obj = self.queue.pop(0)
+        if len(self.queue) == 0:
+            self.nemptyevent.clear()
+        self.lock.release()
+        return obj
