0011bb14bfa7922df01c0fc18de7d009591acf77
unknown <kharitonov@.ld.yandex.ru>
2008-07-12 15:46:52 +0400

    message developed

--- code/messages.py
+++ code/messages.py
@@ -1,11 +1,32 @@
-
-
-
 #message types
 
 INIT = 'I'
+
+# INIT_MESSAGE = ( 'I', (    dx, dy, time_limit, min_sensor, max_sensor, max_speed, max_turn, max_turn_hard    )  )
+#
+
+
 TELE = 'T'
+#TELE_MESSAGE = ('T', (
+#
 END = 'E'
+# END_MESSAGE = ('E', (time, score))
+#
 
 DIE = 'D'
+#DIE_MESSAGE = ('D', (reason, timestamp)
+#
 SUCCESS = 'S'
+#SUCCESS_MESSAGE = ('S', (timestamp))
+#
+
+TERMINATE = 'X'
+
+#
+#TERMINATE_MESS = ('X',)
+
+
+# if message[0] == messages.TERMINATE:
+    #exit
+#elif message[0] == messages.TELE:
+    #s'df'dskf'ksdf
