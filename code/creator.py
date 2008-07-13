8adb38976576000ad236428a5be99d1bfbbe49f2
unknown <kharitonov@.ld.yandex.ru>
2008-07-13 23:45:51 +0400

    new features and bugfixed

--- code/creator.py
+++ code/creator.py
@@ -42,8 +42,8 @@ class Creator(Thread):
             elif msg[0] == messages.INIT:
                 print 'Creator: initing'
                 self.x, self.y = msg[1][0],msg[1][1]
-                self.recalculate()
-                self.sendWaypoint()
+                #self.recalculate()
+                #self.sendWaypoint()
             elif msg[0] == messages.END:
                 print 'Creator: end. New world creating'
                 self.world_container = WorldContainer()
@@ -73,44 +73,71 @@ class Creator(Thread):
             object_2_distance.sort(cmp = cmp_f)
             print 'Creator: objsts', object_2_distance
             for i in xrange(len(object_2_distance)):
-                obstacle = object_2_distance
-                print 'Creator: trying to avoid obstacle', obstacle
-                local = getPointToSearch(object, self.global_target, obstacle_radius)
+                obstacle = object_2_distance[i]
+                obstacle_radius = self.world_container.hashmap[obstacle[0]][1]
+                print 'Creator: trying to avoid obstacle', obstacle, 'with radius', obstacle_radius
+                local = self.getPointToSearch(object, self.global_target, obstacle_radius)
                 print 'Creator: trying to avoid through' , local
-                nearest1 =  geom.fromPoint2Line(object_2_distance[(i+1) % len(object_2_distance)], object_2_distance[object][1], self.radius, (self.x, self.y), local)
-                nearest2 =  geom.fromPoint2Line(object_2_distance[(i-1) % len(object_2_distance)], object_2_distance[object][1], self.radius, (self.x, self.y), local)
+                
+                nearest_object_1 = object_2_distance[(i+1) % len(object_2_distance)][0]
+                nearest_object_1_radius = self.world_container.hashmap[nearest_object_1][1]
+                
+                nearest_object_2 = object_2_distance[(i-1) % len(object_2_distance)][0]
+                nearest_object_2_radius = self.world_container.hashmap[nearest_object_1][1]
+                
+                nearest1 =  geom.fromPoint2Line(nearest_object_1, nearest_object_1_radius, self.radius, (self.x, self.y), local)
+                nearest2 =  geom.fromPoint2Line(nearest_object_2, nearest_object_2_radius, self.radius, (self.x, self.y), local)
                 
                 if (not (nearest1[1] and nearest1[0])) and (not (nearest1[1] and nearest1[0])):
+                    print nearest1
+                    print nearest2
                     print 'Creator: no another obstacles found'
                     self.local_target = local
                     print 'Creator: going to', local
                     break
+                else:
+                    print nearest1
+                    print nearest2
+                    print 'anther obstacles found', nearest_object_1,nearest_object_2
             #nearest are at top
             
     def getPointToSearch(self, obstacle_coords, target_coords, obstacle_radius):
-        print 'Creator: trying to avoid obstacle %s, while trying to get to %s'%(obstacle_coords, target_coords)
-        obstacle_x, obstacle_y = obstacle_coords
-        target_x, target_y = target_coords
-        
-        vector_2_target = target_x - self.x, target_y - self.y
-        vector_2_obstacle = obstacle_x - self.x, obstacle_y - self.y
-        
-        dot_product = vector_2_target[0]*vector_2_obstacle[0]+vector_2_target[0]*vector_2_obstacle[0]
-        
-        normal_vector_x =  -vector_2_obstacle[0] + dot_product * vector_2_target[0]
-        normal_vector_y =  -vector_2_obstacle[1] + dot_product * vector_2_target[1]
-        
-        if (normal_vector_x == 0) and (normal_vector_y == 0):
-            normal_vector_x, normal_vector_y = -vector_2_obstacle_y,vector_2_obstacle_x
-            
-        norma = (normal_vector_x**2 + normal_vector_y**2)**0.5
-        
-        normal_vector_x, normal_vector_y = normal_vector_x/norma, normal_vector_y/norma
+        #def searchPoint(source, obstacle, obstacle_radius, source_radius, bleft):
         
         
-        delta_x, delta_y  = normal_vector_x * (obstacle_radius + self.x), normal_vector_y * (obstacle_radius + self.x)
-        result =  obstacle_x + delta_x, obstacle_y + delta_y
-        print 'Creator: trying to get to %s'%result
+        result =  geom.searchPoint((self.x, self.y), obstacle_coords, obstacle_radius, self.radius, False)
+        print 'Creator: Trying to avoid while going to local waypoint', result
+        #print 'Creator: trying to avoid obstacle %s, while trying to get to %s'%(obstacle_coords, target_coords)
+        #obstacle_x, obstacle_y = obstacle_coords
+        #target_x, target_y = target_coords
+        
+        #vector_2_target = target_x - self.x, target_y - self.y
+        #vector_2_obstacle = obstacle_x - self.x, obstacle_y - self.y
+        #modulo = vector_2_target[0]*vector_2_target[0] + vector_2_target[1]*vector_2_target[1]
+        
+        #dot_product = vector_2_target[0]*vector_2_obstacle[0]+vector_2_target[0]*vector_2_obstacle[0]
+        
+        #dot_product = dot_product/modulo
+        #print 'normalized dot product is ', dot_product
+        #normal_vector_x =  -vector_2_obstacle[0] + dot_product * vector_2_target[0]
+        #normal_vector_y =  -vector_2_obstacle[1] + dot_product * vector_2_target[1]
+        
+        #print 'Creator: projection', dot_product * vector_2_target[0], dot_product * vector_2_target[1]
+        #print 'Creator: normal vector', normal_vector_x, normal_vector_y
+        
+        #if (normal_vector_x == 0) and (normal_vector_y == 0):
+        #    print 'Creator: vector are collinear'
+        #    normal_vector_x, normal_vector_y = -vector_2_obstacle[0], vector_2_obstacle[1]
+        #    print 'Creator: taking normal as', normal_vector_x, normal_vector_y
+        #norma = (normal_vector_x**2 + normal_vector_y**2)**0.5
+        
+        #normal_vector_x, normal_vector_y = normal_vector_x/norma, normal_vector_y/norma
+        
+        
+        #delta_x, delta_y  = normal_vector_x * (1.5*obstacle_radius + self.radius), normal_vector_y * (1.5*obstacle_radius + self.radius)
+        #print 'Creator: delta is', delta_x, delta_y
+        #result =  obstacle_x + delta_x, obstacle_y + delta_y
+        #print 'Creator: trying to get to',result
         return result
         
 class WorldContainer():
