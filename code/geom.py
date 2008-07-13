b427109fa54f7da360c66459156846cb880a6e3c
unknown <kharitonov@.ld.yandex.ru>
2008-07-13 11:40:49 +0400

    fixes, new function added

--- code/geom.py
+++ code/geom.py
@@ -1,5 +1,21 @@
 import math
-            
+def fromPoint2Line(dist_point, dist_radius, rover_radius, line_point1, line_point2):
+    if (line_point1[1] == line_point2[1]) :
+        a = 0.0
+        b = 1.0
+        c = line_point1[1]
+    else:
+        a = 1.0
+        b = (line_point1[0] - line_point2[0])/(line_point1[1] - line_point2[1])
+        c = -line_point1[0] - b*line_point1[1]
+        b = -b    
+    dist = (a*dist_point[0] + b*dist_point[1]+ c)/((a*a + b*b)**0.5)
+    print 'a,b,c', a,b,c
+    if abs(dist) > (rover_radius + dist_radius):
+        return (False, dist)
+    return (True, dist)
+        
+        
 class MPoint:
     def __init__(self, x, y, r):
         self.x = x
@@ -45,8 +61,21 @@ class MLine:
         print self.a, self.b, self.c
 if __name__ == '__main__':
     rover      = MPoint(0.0, 0.0, 0.2)
-    obstacle = MPoint(1.0, 1000.0, 0.3)
-    destination = MPoint(1.0, 1.0, 0.0)
+    obstacle = MPoint(1.0, 1.0, 0.3)
+    destination = MPoint(-2.0, 2.0, 0.0)
     
     b, d = rover.isObstacleInStripe(destination, obstacle)
     print b, d
+    obstacle = (1.0, 1.0)
+    obstacle_radius = 1
+    
+    rover = (0.0, 0.0)
+    rover_radius = 0.2
+    
+    target = (2.0, 2.0)
+    
+    
+    
+    b, d = fromPoint2Line(obstacle, obstacle_radius, rover_radius, rover, target)#rover.isObstacleInStripe(destination, obstacle)
+    print b, d
+    
